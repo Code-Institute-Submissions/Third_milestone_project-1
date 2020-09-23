52f995e3c5eeea5d0da5c2ef24dd48fe4ce8f735
plxsas <sale12302006@yahoo.com>
2020-09-23 16:16:09 +0000

    things are better now

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ mongo = PyMongo(app)
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("books.html", 
-                           books=mongo.db.Books.find())
+                           books=mongo.db.Books.find(),names=mongo.db.Books.find(),reviews=mongo.db.Books_reviews.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),

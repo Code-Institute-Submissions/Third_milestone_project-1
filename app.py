c26093d4e994b0b934c9b0ae39f7184af0745023
plxsas <sale12302006@yahoo.com>
2020-10-14 09:43:43 +0000

    more changes

--- app.py
+++ app.py
@@ -10,37 +10,7 @@ app.config["MONGO_DBNAME"] = 'task_manager'
 app.config["MONGO_URI"] = 'mongodb+srv://plxsas:Salha2019@cluster1.cqq5g.mongodb.net/Books_list?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
-GoogleMaps(app, key="ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
-@app.route("/")
-def mapview():
-    # creating a map in the view
-    mymap = Map(
-        identifier="view-side",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[(37.4419, -122.1419)]
-    )
-    sndmap = Map(
-        identifier="sndmap",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-             'lat': 37.4419,
-             'lng': -122.1419,
-             'infobox': "<b>Hello World</b>"
-          },
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-             'lat': 37.4300,
-             'lng': -122.1400,
-             'infobox': "<b>Hello World from other place</b>"
-          }
-        ]
-    )
-    return render_template('contact.html', mymap=mymap, sndmap=sndmap)
+GoogleMaps(app)
 
 @app.route('/')
 @app.route('/get_books')
